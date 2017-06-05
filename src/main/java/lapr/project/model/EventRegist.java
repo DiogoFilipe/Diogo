e72d6173a202fe6344894e3ca889ba1fd4f27d7e
João Sousa <1160795@isep.ipp.pt>
2017-06-05 10:22:02 +0100

    Refactored Regist classes. Changed class comments.

--- src/main/java/lapr/project/model/EventRegist.java
+++ src/main/java/lapr/project/model/EventRegist.java
@@ -4,6 +4,6 @@ package lapr.project.model;
  *
  * @author 1160590_1160795_1160844_1161241_1162109
  */
-public class User {
+public class EventRegist {
     
 }
--- src/main/java/lapr/project/model/FairCenter.java
+++ src/main/java/lapr/project/model/FairCenter.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lapr.project.model;
 
 /**
  *
- * @author Diogo
+ * @author 1160590_1160795_1160844_1161241_1162109
  */
 public class FairCenter {
     
