79f97735104eed0763fb29399c7ef9a57b571c04
Berto Alves <1160844@isep.ipp.pt>
2017-06-05 10:46:12 +0100

    [Implementation] -  started the implementation of the user class

--- src/main/java/lapr/project/model/User.java
+++ src/main/java/lapr/project/model/User.java
@@ -1,9 +1,54 @@
 package lapr.project.model;
 
+import java.io.Serializable;
+
 /**
  *
  * @author 1160590_1160795_1160844_1161241_1162109
  */
-public class User {
+public class User implements Serializable{
+    
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+    
+    public User(String nome,String username,String email,String password){
+        this.name = nome;
+        this.username = username;
+        this.email = email;
+        //this.password = password; - Password needs to be stored encrypted
+    }
+
+    /**
+     * Returns the User's name
+     * @return User's name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Returns the User's username
+     * @return User's username
+     */
+    public String getUsername() {
+        return username;
+    }
+    
+    /**
+     * Returns the User's email
+     * @return User's email
+     */
+    public String getEmail() {
+        return email;
+    }
+       
+    @Override
+    public String toString() {
+        return String.format("Name: %s %nUsername: %s %nEmail: %s%n", name, username, email);
+    }
+    
+    //Password related methods
     
 }
