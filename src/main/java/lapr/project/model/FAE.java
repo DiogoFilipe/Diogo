44801e5fa34c066fc32fe3d40d493d53ffdce237
Diogo <gui.nho0098@gmail.com>
2017-06-06 10:14:21 +0100

    FAE class created and completed with constructor, gets and toString

---
+++ src/main/java/lapr/project/model/FAE.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Diogo
+ */
+public class FAE extends User implements Serializable{
+    
+    /**
+     * constructor
+     * @param name is the name of the FAE
+     * @param username is the FAE's name in the platform
+     * @param email is the e-mail of the FAE
+     * @param password is the FAE's password
+     */
+    public FAE(String name,String username,String email,String password){
+        super(name,username,email,password);
+    }
+
+    /**
+     * 
+     * @return the FAE's e-mail 
+     */
+    @Override
+    public String getEmail() {
+        return super.getEmail();
+    }
+
+    /**
+     * 
+     * @return the FAE's name 
+     */
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    /**
+     * 
+     * @return the FAE's name in the platform
+     */
+    @Override
+    public String getUsername() {
+        return super.getUsername();
+    }
+    
+    
+
+    /**
+     * 
+     * @return all info of the FAE, except the password 
+     */
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
