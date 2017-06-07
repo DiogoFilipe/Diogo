c49934c127d03805ed688827495140a3ef217e4e
Diogo <gui.nho0098@gmail.com>
2017-06-07 11:41:32 +0100

    removed some methods that were wrong

--- src/main/java/lapr/project/model/FAE.java
+++ src/main/java/lapr/project/model/FAE.java
@@ -11,42 +11,34 @@ import java.io.Serializable;
  *
  * @author Diogo
  */
-public class FAE extends User implements Serializable{
-    
-    /**
-     * utilizador
-     */
-    private User u;
-    
+public class FAE extends User implements Serializable {
+
     /**
      * experiência profissional em organização de eventos
      */
-    private int professionalexperience;
-    
+    private int professionalExperience;
+
     /**
      * constructor
+     *
      * @param name is the name of the FAE
      * @param username is the FAE's name in the platform
      * @param email is the e-mail of the FAE
      * @param password is the FAE's password
+     * @param professionalExperience
      */
-    public FAE(String name,String username,String email,String password){
-        super(name,username,email,password);
+    public FAE(String name, String username, String email, String password, int professionalExperience) {
+        super(name, username, email, password);
+        this.professionalExperience = professionalExperience;
     }
-    
-     /**
-     * Construtor
-     * @param u
-     * @param exp 
-     */
-    public FAE(User u, int exp) {
-        this.u = u;
-        this.professionalexperience=exp;
+
+    public FAE() {
+        super();
     }
 
     /**
-     * 
-     * @return the FAE's e-mail 
+     *
+     * @return the FAE's e-mail
      */
     @Override
     public String getEmail() {
@@ -54,8 +46,8 @@ public class FAE extends User implements Serializable{
     }
 
     /**
-     * 
-     * @return the FAE's name 
+     *
+     * @return the FAE's name
      */
     @Override
     public String getName() {
@@ -63,30 +55,20 @@ public class FAE extends User implements Serializable{
     }
 
     /**
-     * 
+     *
      * @return the FAE's name in the platform
      */
     @Override
     public String getUsername() {
         return super.getUsername();
     }
-    
-    
 
     /**
-     * 
-     * @return all info of the FAE, except the password 
+     *
+     * @return all info of the FAE, except the password
      */
     @Override
     public String toString() {
         return super.toString();
-    }
-    
-    /**
-     * Method that allows to obtain the username of a certain Fae
-     * @return 
-     */
-    public String getFaeUsername(){
-        return u.getUsername();
     }
 }
