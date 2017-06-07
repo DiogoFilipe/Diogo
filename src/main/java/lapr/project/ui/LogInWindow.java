75c1d0edbcc8d71e1d6868a9891536f3c1bfae05
Diogo <gui.nho0098@gmail.com>
2017-06-07 11:35:51 +0100

    LogIn with the refer to MainWindow

--- src/main/java/lapr/project/ui/LogInWindow.java
+++ src/main/java/lapr/project/ui/LogInWindow.java
@@ -5,11 +5,18 @@
  */
 package lapr.project.ui;
 
+import java.util.List;
+import lapr.project.model.FairCenter;
+import lapr.project.model.User;
+import lapr.project.model.UserRegist;
+
 /**
  *
  * @author Diogo
  */
 public class LogInWindow extends javax.swing.JFrame {
+
+    FairCenter fc;
 
     public LogInWindow() {
         initComponents();
@@ -24,12 +31,24 @@ public class LogInWindow extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jFrame1 = new javax.swing.JFrame();
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
         jPasswordField1 = new javax.swing.JPasswordField();
         jTextField1 = new javax.swing.JTextField();
+
+        javax.swing.GroupLayout jFrame1Layout = new javax.swing.GroupLayout(jFrame1.getContentPane());
+        jFrame1.getContentPane().setLayout(jFrame1Layout);
+        jFrame1Layout.setHorizontalGroup(
+            jFrame1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        jFrame1Layout.setVerticalGroup(
+            jFrame1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -117,7 +136,9 @@ public class LogInWindow extends javax.swing.JFrame {
     }//GEN-LAST:event_jPasswordField1ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-
+        MainWindow mainWindow = new MainWindow(fc);
+        mainWindow.setVisible(true);
+        dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
@@ -158,6 +179,7 @@ public class LogInWindow extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JFrame jFrame1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
