de9634cefc66eec608a2f43fc23d0fa114e575e0
Diogo <gui.nho0098@gmail.com>
2017-06-14 17:27:21 +0100

    [Implementation] - ListApplicationsController completed

--- src/main/java/lapr/project/controller/ListApplicationsController.java
+++ src/main/java/lapr/project/controller/ListApplicationsController.java
@@ -1,14 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lapr.project.controller;
+
+import java.util.List;
+import lapr.project.model.Application;
+import lapr.project.model.Event;
+import lapr.project.model.EventRegist;
+import lapr.project.model.Organizer;
 
 /**
  *
  * @author Diogo
  */
 public class ListApplicationsController {
+    
+    private EventRegist eventRegist;
+    
+    /**
+     *
+     * @param o the organizer
+     * @return the list of events of this organizer
+     */
+    public List<Event>  getOrganizerEventList(Organizer o){
+        return  eventRegist.getOrganizerEventsRegist(o);
+    }
+    
+    /**
+     * 
+     * @param e the event
+     * @return the list of applications
+     */
+    public List<Application> getApplicationsList(Event e){
+        return e.getApplicationList();
+    }    
+    
     
 }
