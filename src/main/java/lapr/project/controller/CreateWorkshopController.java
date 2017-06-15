dbe0f0509b2d4d1c3de2964ae2cbf5ca3d76b639
Diogo <gui.nho0098@gmail.com>
2017-06-15 16:13:58 +0100

    [Implementation] - CreateWorkshop main window created

---
+++ src/main/java/lapr/project/controller/CreateWorkshopController.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.controller;
+
+import java.util.List;
+import lapr.project.model.Congress;
+import lapr.project.model.Organizer;
+
+/**
+ *
+ * @author Diogo
+ */
+public class CreateWorkshopController {
+    
+    public List<Congress> getListCongressesOrganizer(Organizer o){
+        
+        return 
+    }
+    
+}
