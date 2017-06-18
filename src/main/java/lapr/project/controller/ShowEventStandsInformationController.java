e7802757cb878f1c28ce8daea802c0a9d5cdc62c
João Sousa <Joo@192.168.1.65>
2017-06-18 01:01:49 +0100

    [Implementation] [UC41] - Added the computation of the mean deviation of the Stands' area to the UC41's Controller and the method that returns the list of Events a User is an Organizer of

--- src/main/java/lapr/project/controller/ShowEventStandsInformationController.java
+++ src/main/java/lapr/project/controller/ShowEventStandsInformationController.java
@@ -1,20 +1,24 @@
 package lapr.project.controller;
 
+import java.util.List;
+import lapr.project.model.*;
+
 /**
  *
  * @author 1160590_1160795_1160844_1161241_1162109
  */
 public class ShowEventStandsInformationController {
     
-    public List<Event> getOrganizerEventList() {
-        
-    }
+    EventRegist er;
+    
+//    public List<Event> getOrganizerEventList() {
+//        return er.getOrganizerEventList();
+//    }
     
     public List<Stand> getEventStandsInformation(Event e) {
         
         return e.getStandList();
         
-        //This goes in the UI, not here
         /*
         List<Stand> standList = e.getStandList();
         List<Double> standsAreaFrom0to10 = new ArrayList<>();
@@ -33,10 +37,10 @@ public class ShowEventStandsInformationController {
             if (stand.getArea() < 10) {
                 standsAreaFrom0to10.add(stand.getArea());
             }
-            if (stand.getArea() >= 10 && stand.area < 20) {
+            if (stand.getArea() >= 10 && stand.getArea() < 20) {
                 standsAreaFrom10to20.add(stand.getArea());
             }
-            if (stand.getArea() >= 20 && stand.area < 30) {
+            if (stand.getArea() >= 20 && stand.getArea() < 30) {
                 standsAreaFrom20to30.add(stand.getArea());
             }
             if (stand.getArea() >= 30) {
@@ -45,7 +49,13 @@ public class ShowEventStandsInformationController {
         }
         
         averageArea = totalArea / nStands;
-        //mean deviation calculation
+        
+        double aux1 = 0, aux2 = 0;
+        for (Stand stand : standList) {
+            aux1 = Math.pow(Math.abs(stand.getArea() - averageArea), 2);
+            aux2 += aux1; 
+        }
+        double meanDeviation = Math.sqrt(aux2 / nStands);
         */
         
     }
