49a5875d7c35be2b6240349a2774db24c967e563
Diogo <gui.nho0098@gmail.com>
2017-06-20 15:01:46 +0100

    [Implementation] - minor changes

--- src/main/java/lapr/project/model/EncryptionList.java
+++ src/main/java/lapr/project/model/EncryptionList.java
@@ -1,92 +1,92 @@
-
 package lapr.project.model;
 
 import java.util.ArrayList;
 import java.util.List;
+
 /**
  *
  * @author 1160590_1160795_1160844_1161241_1162109
  */
 public class EncryptionList {
-    
-   
-    
+
     private List<Encryption> encryptionsList;
-    private static EncryptionList el = new EncryptionList();
-    
-   
+    private EncryptionList el = new EncryptionList();
+
     /**
      * Constructor
      */
     public EncryptionList() {
         encryptionsList = new ArrayList<>();
     }
-    
+
     /**
      * Returns the List of Encryptions
+     *
      * @return List of Encryptions
      */
-    public List<Encryption> getEncryptionsList(){
+    public List<Encryption> getEncryptionsList() {
         return encryptionsList;
     }
-    
-    public static List<Encryption> getEncryptions(){
-    List <Encryption> encryptions = el.getEncryptionsList();
-    return encryptions;
+
+    public List<Encryption> getEncryptions() {
+        List<Encryption> encryptions = el.getEncryptionsList();
+        return encryptions;
     }
+
     /**
      * Defines the Encryptions List
-     * @param Encryptions List 
+     *
+     * @param encryptionsList
      */
-    public void setEncryptionList(List<Encryption> encryptionsList){
+    public void setEncryptionList(List<Encryption> encryptionsList) {
         this.encryptionsList = encryptionsList;
     }
-    
+
     /**
      * Adds a new Encryption to the EncryptionsList
-     * @param fae - Encryption to be added to the EncryptionsList
+     *
+     * @param encryption
      * @return The EncryptionsList
      */
-    public boolean addEncryption(Encryption encryption){
-        if(encryptionsList.contains(encryption)){
+    public boolean addEncryption(Encryption encryption) {
+        if (encryptionsList.contains(encryption)) {
             return false;
         }
         return encryptionsList.add(encryption);
     }
-    
-       public static boolean  registEncryption(Encryption encryption){
-        boolean check = false;
-        if(el.addEncryption(encryption));{
-        check = true;
-        }
-        return check;
+
+    public boolean registEncryption(Encryption encryption) {
+        return el.addEncryption(encryption);
     }
+
     /**
      * Verefies if there is any encryption
-     * @return 
+     *
+     * @return
      */
-     public boolean isEmpty() {
+    public boolean isEmpty() {
         return encryptionsList.isEmpty();
     }
-     
+
     /**
      * check if the list already contains encryptions
+     *
      * @param encryption that will check if is on the list
      * @return true if the encryption already is on the list
      */
-     public boolean containsEncryption(Encryption encryption){
-         return encryptionsList.contains(encryption);
-     }
-    
-     
+    public boolean containsEncryption(Encryption encryption) {
+        return encryptionsList.contains(encryption);
+    }
+
     /**
      * Returns the shift of a given encryption
-     * @param user -  user´s encryption
+     *
+     * @param user - user´s encryption
      * @return the shift
      */
-      public int getShift(User user){
-        for(Encryption e : encryptionsList){
-            if(e.getUser().getUsername().equals(user.getUsername())){
+    public int getShift(User user) {
+        for (Encryption e : encryptionsList) {
+            if (e.getUser().getUsername().equals(user.getUsername())) {
                 return e.getShift();
             }
         }
