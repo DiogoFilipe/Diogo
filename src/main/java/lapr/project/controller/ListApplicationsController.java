3171d6f26572cf6875e78fb20309d8393080e71c
Diogo <gui.nho0098@gmail.com>
2017-06-20 16:26:20 +0100

    [Implementation] - listApplicationsController minor changes

--- src/main/java/lapr/project/controller/ListApplicationsController.java
+++ src/main/java/lapr/project/controller/ListApplicationsController.java
@@ -1,10 +1,8 @@
 package lapr.project.controller;
 
 import java.util.List;
-import lapr.project.model.EventRegist;
 import lapr.project.model.FairCenter;
 import lapr.project.model.Organizer;
-import lapr.project.model.OrganizerList;
 import lapr.project.model.User;
 
 /**
@@ -13,8 +11,6 @@ import lapr.project.model.User;
  */
 public class ListApplicationsController {
     
-    private EventRegist eventRegist;
-    private OrganizerList organizerList;
     private FairCenter fc;
     private User u;
     
@@ -25,16 +21,16 @@ public class ListApplicationsController {
     }
     
     public boolean isOrganizer(User u){
-        return organizerList.containsOrganizer((Organizer)u);
+        return fc.getOrganizerList().containsOrganizer((Organizer)u);
     }
     
     /**
      *
-     * @param o the organizer
+     * @param u
      * @return the list of events of this organizer
      */
-    public List<String>  getOrganizerEventsList(Organizer o){
-        return  eventRegist.getOrganizerEventsListOrdenedByState(o);
+    public List<String>  getOrganizerEventsList(User u){
+        return  fc.getEventRegist().getOrganizerEventsListOrdenedByState((Organizer)u);
     }
     
     /**
