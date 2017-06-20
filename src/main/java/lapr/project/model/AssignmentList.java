94008d9f6742c58b2acd0168c15c4b98c0012510
João Sousa <1160795@isep.ipp.pt>
2017-06-21 00:16:20 +0100

    [Implementation] - Refactored assignmentsList to assignmentList in the AssignmentList class

--- src/main/java/lapr/project/model/AssignmentList.java
+++ src/main/java/lapr/project/model/AssignmentList.java
@@ -10,61 +10,64 @@ import java.util.List;
 
 /**
  *
- * @author HP
+ * @author 1160590_1160795_1160844_1161241_1162109
  */
 public class AssignmentList {
-    
-    private List<Assignment> assignmentsList;
+
+    private List<Assignment> assignmentList;
 
     public AssignmentList() {
-        assignmentsList = new ArrayList<>();
+        assignmentList = new ArrayList<>();
     }
-    
+
     /**
      * Returns the List of assignments
+     *
      * @return Assignment´s list
      */
-     public List<Assignment> getAssignmentsList(){
-        return new ArrayList<>(this.assignmentsList);
+    public List<Assignment> getAssignmentList() {
+        return new ArrayList<>(this.assignmentList);
     }
-     
-     /**
-      * Returns the number of assignments
-      * @return number of assignments
-      */
-     public int getSize(){
-        return assignmentsList.size();
+
+    /**
+     * Returns the number of assignments
+     *
+     * @return number of assignments
+     */
+    public int getSize() {
+        return assignmentList.size();
     }
-     
-     /**
-      * Removes an assignment from the list
-      * @param assignment - assignment to be removed
-      */
-     public void remove(Assignment assignment) {
-        assignmentsList.remove(assignment);
+
+    /**
+     * Removes an assignment from the list
+     *
+     * @param assignment - assignment to be removed
+     */
+    public void remove(Assignment assignment) {
+        assignmentList.remove(assignment);
     }
-     
-     /**
-      * Adds an assignment to the assignment List
-      * @param assignment - assignment to be added
-      * @return 
-      */
-     public boolean registAssignment(Assignment assignment) {
-        if(!assignmentsList.contains(assignment)){
-            assignmentsList.add(assignment);
+
+    /**
+     * Adds an assignment to the assignment List
+     *
+     * @param assignment - assignment to be added
+     * @return
+     */
+    public boolean registAssignment(Assignment assignment) {
+        if (!assignmentList.contains(assignment)) {
+            assignmentList.add(assignment);
             return true;
         }
         return false;
     }
-     
-     /**
-      * Verefies if there are any assignments
-      * @return 
-      */
-      public boolean isEmpty(){
-        return assignmentsList.isEmpty();
+
+    /**
+     * Verefies if there are any assignments
+     *
+     * @return
+     */
+    public boolean isEmpty() {
+        return assignmentList.isEmpty();
     }
-    
-     
-     
+
 }
