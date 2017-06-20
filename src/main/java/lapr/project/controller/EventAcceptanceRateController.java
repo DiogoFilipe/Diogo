de92be6c2f6a8320e4acebe558cb52e894cef873
Diogo <gui.nho0098@gmail.com>
2017-06-21 00:59:58 +0100

    [Implementation] - event acceptance rate done

---
+++ src/main/java/lapr/project/controller/EventAcceptanceRateController.java
@@ -0,0 +1,38 @@
+package lapr.project.controller;
+
+import java.util.List;
+import lapr.project.model.FairCenter;
+import lapr.project.model.User;
+
+/**
+ *
+ * @author Diogo
+ */
+public class EventAcceptanceRateController {
+
+    FairCenter fc;
+    User u;
+
+    public EventAcceptanceRateController(FairCenter fc, User u) {
+        this.fc = fc;
+        this.u = u;
+    }
+    
+    public List<String> getEventList(){
+        return fc.getEventRegist().getEventListString();
+    }
+
+    public List<String> getApplicationsDecided(String e) {
+        return fc.getEvent(e).getApplicationList().getApplicationListAccepted();
+    }
+
+    public List<String> getApplicationsRejected(String e) {
+        return fc.getEvent(e).getApplicationList().getApplicationListRejected();
+    }
+    
+    public double acceptanceRate(String e){
+        List<String> accepted = getApplicationsDecided(e);
+        List<String> rejected = getApplicationsRejected(e);
+        return (accepted.size()/(rejected.size()+accepted.size()))*100;
+    }
+}
