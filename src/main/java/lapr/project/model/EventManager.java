796b94128ea17543d987c0df0703dbb31b7cd5e9
Diogo <gui.nho0098@gmail.com>
2017-06-21 00:01:20 +0100

    [Implementation] - creation of the classe EventManager

---
+++ src/main/java/lapr/project/model/EventManager.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.model;
+
+/**
+ *
+ * @author Diogo
+ */
+public class EventManager extends User{
+    
+    public EventManager(String name,String username,String email,String password){
+        super(name,username,email,password);
+    }
+
+    @Override
+    public String getEmail() {
+        return super.getEmail(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getName() {
+        return super.getName(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getPassword() {
+        return super.getPassword(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean hasTheUsername(User user, String username) {
+        return super.hasTheUsername(user, username); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getUsername() {
+        return super.getUsername(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setName(String name) {
+        super.setName(name); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setEmail(String email) {
+        super.setEmail(email); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setPassword(String password) {
+        super.setPassword(password); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setUsername(String username) {
+        super.setUsername(username); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public EventManager() {
+    }
+
+    @Override
+    public String toString() {
+        return super.toString(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+    
+}
