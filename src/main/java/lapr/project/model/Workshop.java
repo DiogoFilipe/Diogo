6e329e04bdcd56b805ca3075de2f13d315e78da1
Diogo <gui.nho0098@gmail.com>
2017-06-21 05:04:42 +0100

    [Implementation] - added some gets/sets/javaDocs and class created

---
+++ src/main/java/lapr/project/model/Workshop.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.model;
+
+/**
+ *
+ * @author Diogo
+ */
+public class Workshop {
+    
+    /**
+     * title of the workshop
+     */
+    private String title;
+    
+    /**
+     * description of the workshop
+     */
+    private String description;
+    
+    public Workshop(String title,String description){
+      this.title=title;
+      this.description=description;
+    }
+
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    } 
+}
