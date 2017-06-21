ce4b4c7379838e835896dad6f14ed92ceeeb7670
Diogo <gui.nho0098@gmail.com>
2017-06-21 21:38:57 +0100

    [Implementation] - added javadocs //fixed errors

--- src/main/java/lapr/project/model/Organizer.java
+++ src/main/java/lapr/project/model/Organizer.java
@@ -7,75 +7,96 @@ import java.io.Serializable;
  * @author 1160590_1160795_1160844_1161241_1162109
  */
 public class Organizer extends User implements Serializable {
-    
-    private User user;
-    
-    public Organizer(User user){
-        this.user = user;
-    }
-    
-    public Organizer(String name,String username,String email,String password){
-        super(name,username,email,password);
-    }
-        
-    public void setUser(User user){
-        this.user = user;
-    }
-    
-   
-    
-    /**Verifies if the two users are equal
-     * 
-     * @param user - user to compare
-     * @return 
-     */
-    public boolean isUser(User user){
-        if(this.getUser() != null){
-            return this.getUser().equals(user);
-        }
-        return false;
-    }
-    
-        @Override
-    public String toString(){
-        return getUser() != null ? getUser().toString() : null;
-    }
-    
-  /**Validates object, verifying what type it is and if it exists
-   * 
-   * @param object - object to compare
-   * @return
-   */
-    @Override
-    public boolean equals(Object object) {
-        if(this == object) {
-            return true;
-        }
-        
-        if (object == null || getClass() != object.getClass()) {
-            return false;
-        }
-        
-        Organizer otherOrganizer = (Organizer) object;
-        return otherOrganizer.isUser(getUser());
+
+    public Organizer(String name, String username, String email, String password) {
+        super(name, username, email, password);
     }
 
     /**
-     * @return the user
+     *
+     * @return all data about organizer
      */
-    public User getUser() {
-        return user;
+    @Override
+    public String toString() {
+        return super.toString();
     }
 
-   /**
-    * 
-    * @return the name of the user 
-    */
+    /**
+     *
+     * @return the name of the user
+     */
     @Override
     public String getName() {
-        return super.getName(); 
+        return super.getName();
     }
-    
-    
-    
+
+    /**
+     *
+     * @return organizer's passowrd
+     */
+    @Override
+    public String getPassword() {
+        return super.getPassword();
+    }
+
+    /**
+     *
+     * @return organizer's email
+     */
+    @Override
+    public String getEmail() {
+        return super.getEmail();
+    }
+
+    /**
+     *
+     * @return organizer's username
+     */
+    @Override
+    public String getUsername() {
+        return super.getUsername();
+    }
+
+    /**
+     *
+     * @param email organizer's email
+     */
+    @Override
+    public void setEmail(String email) {
+        super.setEmail(email);
+    }
+
+    /**
+     *
+     * @param name organizer's name
+     */
+    @Override
+    public void setName(String name) {
+        super.setName(name);
+    }
+
+    /**
+     *
+     * @param password organizer's password
+     */
+    @Override
+    public void setPassword(String password) {
+        super.setPassword(password);
+    }
+
+    /**
+     *
+     * @param username organizer's username
+     */
+    @Override
+    public void setUsername(String username) {
+        super.setUsername(username);
+    }
+
+    /**
+     * empty constructor
+     */
+    public Organizer() {
+    }
+
 }
