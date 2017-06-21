c8ec84c6d38e2088b597dc341204b560f7d3f8e8
Diogo <gui.nho0098@gmail.com>
2017-06-21 17:59:12 +0100

    [Implementation] - added javadocs/gets/sets

--- src/main/java/lapr/project/model/FAE.java
+++ src/main/java/lapr/project/model/FAE.java
@@ -27,25 +27,37 @@ public class FAE extends User implements Serializable {
      * @param username is the FAE's name in the platform
      * @param email is the e-mail of the FAE
      * @param password is the FAE's password
-     * @param professionalExperience
+     * @param professionalExperience his professional experience
      * @param decision made by the fae
      */
-    public FAE(String name, String username, String email, String password, int professionalExperience,Decision decision) {
+    public FAE(String name, String username, String email, String password, int professionalExperience, Decision decision) {
         super(name, username, email, password);
         this.professionalExperience = professionalExperience;
         this.decision = decision;
     }
-    
-       public FAE(String name, String username, String email, String password) {
-        super(name, username, email, password);}
+
+    /**
+     * 
+     * @param name name of fae
+     * @param username name in platform of fae
+     * @param email email of the fae
+     * @param password his password
+     */
+    public FAE(String name, String username, String email, String password) {
+        super(name, username, email, password);
+    }
+
     /**
      *
-     * @param u
+     * @param u an user
      */
     public FAE(User u) {
         this.user = u;
     }
 
+    /**
+     * empty constructor
+     */
     public FAE() {
         super();
     }
@@ -85,6 +97,7 @@ public class FAE extends User implements Serializable {
     public String toString() {
         return super.toString();
     }
+
     /**
      *
      * @return
