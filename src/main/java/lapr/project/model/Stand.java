c957f3c200d43e5dcd4ed0dca48e502baa9f611e
Diogo <gui.nho0098@gmail.com>
2017-06-21 22:24:47 +0100

    [Implementation] - added javadocs //fixed errors

--- src/main/java/lapr/project/model/Stand.java
+++ src/main/java/lapr/project/model/Stand.java
@@ -6,72 +6,96 @@ import java.io.Serializable;
  *
  * @author 1160590_1160795_1160844_1161241_1162109
  */
-public class Stand implements Serializable{
-    
+public class Stand implements Serializable {
+
+    /**
+     * name of the stand
+     */
     private String id;
+
+    /**
+     * area of the stand
+     */
     private double area;
-    
+
+    /**
+     * number of stands
+     */
     private static int totalStands = 0;
+
+    /**
+     * prefix of the stand name
+     */
     private static String ID_PREFIX = "Stand";
-    
+
     /**
      * Constructor for Stand
+     *
      * @param area Stand's area
      */
     public Stand(double area) {
         this.id = ID_PREFIX + (totalStands++);
         this.area = area;
     }
-    
+
     /**
      * Returns the Stand's ID
+     *
      * @return Stand's ID
      */
     public String getID() {
         return id;
     }
-    
+
     /**
      * Returns the Stand's area
+     *
      * @return Stand's area
      */
     public double getArea() {
         return area;
     }
-    
+
     /**
      * Modifies the Stand's ID
+     *
      * @param id Stand's ID
      */
     public void setID(String id) {
         this.id = id;
     }
-    
+
     /**
      * Modifies the Stand's area
-     * @param area 
+     *
+     * @param area
      */
     public void setArea(double area) {
         this.area = area;
     }
-    
-    
-    
+
     /**
      * Returns a textual representation of the Stand
+     *
      * @return textual representation of the Stand
      */
     @Override
     public String toString() {
         return "The stand " + id + " has an area of " + area + "square meters";
     }
-    
-    public static boolean verifyArea(String area){        
-    
-    for (int i = 0; i < area.length(); i++) {
-      if (!Character.isDigit(area.charAt(i)))
-        return false;
+
+    /**
+     * 
+     * @param area area of the stand
+     * @return verify if the area(String) has only digits
+     */
+    public static boolean verifyArea(String area) {
+
+        for (int i = 0; i < area.length(); i++) {
+            if (!Character.isDigit(area.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
     }
-    return true;
-    }
-}    
+}
