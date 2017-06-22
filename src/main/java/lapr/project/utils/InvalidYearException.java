d4fb62904b4c5694fb7928604842fb410a11e774
Diogo <gui.nho0098@gmail.com>
2017-06-22 03:36:49 +0100

    [Implementation] - exception created

---
+++ src/main/java/lapr/project/utils/InvalidYearException.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.utils;
+
+/**
+ *
+ * @author Diogo
+ */
+public class InvalidYearException extends IllegalArgumentException {
+
+    /**
+     * Creates a new instance of <code>InvalidYearException</code> without
+     * detail message.
+     */
+    public InvalidYearException() {
+    }
+
+    /**
+     * Constructs an instance of <code>InvalidYearException</code> with the
+     * specified detail message.
+     *
+     * @param msg the detail message.
+     */
+    public InvalidYearException(String msg) {
+        super("Invalid year on "+msg);
+    }
+}
