1d17b33c4d22a5f5d8ce0f7a4b2094079886bb52
Berto Alves <1160844@isep.ipp.pt>
2017-06-22 17:46:05 +0100

    [Implementation] - ImportXMLFile completed

--- src/main/java/lapr/project/model/Representative.java
+++ src/main/java/lapr/project/model/Representative.java
@@ -1,10 +1,12 @@
 package lapr.project.model;
 
+import java.io.Serializable;
+
 /**
  *
  * @author 1160590_1160795_1160844_1161241_1162109
  */
-public class Representative extends User{
+public class Representative extends User implements Serializable{
     
    /**
     * name of the company
