6441e405f1c8da02539cfbf391c0231bb39eb1e2
João Domingues <Joo Domingues@DESKTOP-R1TQLVA.lan>
2017-06-22 16:36:14 +0100

    [Implementation]- DifferenceTwoEventsAcceptanceRateController

---
+++ src/main/java/lapr/project/controller/DifferenceTwoEventsAcceptanceRateController.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.controller;
+
+import java.util.List;
+import lapr.project.model.FairCenter;
+import lapr.project.model.User;
+
+/**
+ *
+ * @author João Domingues
+ */
+public class DifferenceTwoEventsAcceptanceRateController {
+    
+    FairCenter fc;
+    User u;
+
+    public DifferenceTwoEventsAcceptanceRateController(FairCenter fc, User u) {
+        this.fc = fc;
+        this.u = u;
+    }
+    
+    public List<String> getEventList(){
+        return fc.getEventRegist().getEventListString();
+    }
+
+    public List<String> getApplicationsDecided(String e) {
+        return fc.getEvent(e).getApplicationList().getApplicationListAccepted();
+    }
+
+    public List<String> getApplicationsRejected(String e) {
+        return fc.getEvent(e).getApplicationList().getApplicationListRejected();
+    }
+    
+    public double acceptanceRate(String e){
+        List<String> accepted = getApplicationsDecided(e);
+        List<String> rejected = getApplicationsRejected(e);
+        return (accepted.size()/(rejected.size()+accepted.size()))*100;
+    }
+    
+    public double differenceAcceptanceRate(String e, String evt){
+        return Math.abs((acceptanceRate(e)-(acceptanceRate(evt))));
+    }
+    
+}
