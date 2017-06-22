b95de8ff994372c27b9a943d15a2c6378dd052eb
Diogo <gui.nho0098@gmail.com>
2017-06-22 17:51:45 +0100

    [Implementation] - minor change

--- src/main/java/lapr/project/controller/DecideApplicationController.java
+++ src/main/java/lapr/project/controller/DecideApplicationController.java
@@ -9,43 +9,43 @@ import lapr.project.model.*;
  * @author 1160590_1160795_1160844_1161241_1162109
  */
 public class DecideApplicationController {
-    
+
     FairCenter fc;
     EventRegist er;
     UserRegist ur;
     Application a;
-    FAE fae;
+    User user;
     Decision d;
-    
+
     public DecideApplicationController(FairCenter fc, User user) {
         this.fc = fc;
         this.er = fc.getEventRegist();
         this.ur = fc.getUserRegist();
         this.a = new Application();
-        this.fae = new FAE(user);
+        this.user = user;
     }
-    
+
     /**
      * Returns a list of Events the logged in User belongs to as an FAE
-     * 
+     *
      * @return list of Events the logged in User belongs to as an FAE
      */
     public List<String> getFAEEventList() {
         List<Event> eventList = er.getEventList();
         List<String> eventListString = new ArrayList<>();
         for (Event event : eventList) {
-            if (event.getFAEList().getFAEList().contains(fae)) {
+            if (event.getFAEList().getFAEList().contains((FAE) user)) {
                 eventListString.add(event.getTitle());
             }
         }
         return eventListString;
     }
-    
+
     /**
-     * Returns a list of Events that include the FAE
-     * 
+     * Returns a list of applications that include the FAE
+     *
      * @param eventTitle
-     * @return 
+     * @return
      */
     public List<String> getFAEApplicationsForDecision(String eventTitle) {
         List<Event> eventList = er.getEventList();
@@ -58,48 +58,59 @@ public class DecideApplicationController {
         List<Assignment> assignmentList = e.getAssignmentList().getAssignmentList();
         List<String> applicationListString = new ArrayList<>();
         for (Assignment assignment : assignmentList) {
-            if (assignment.getFAEList().contains(fae) /*&& assignment.getApplication().getState() == ApplicationState.State.Decided*/) {
+            if (assignment.getFAEList().contains((FAE) user) /*&& assignment.getApplication().getState() == ApplicationState.State.Decided*/) {
                 applicationListString.add(assignment.getApplication().getCompanyName());
             }
         }
         return applicationListString;
     }
-    
+
     /**
      * Returns a String containing the Application's details
-     * 
+     *
+     * @param companyName
      * @return String containing the Application's details
      */
-    public String getApplicationDetails() {
+    public String getApplicationDetails(String companyName) {
+
         return a.getCompanyName() + "\n" + a.getAddress() + "\n" + a.getContact() + "\n" + a.getDescription();
     }
-    
+
     /**
      * Sets the Decision the FAE has made
-     * 
-     * @param decision true if the application is accepted / false if the application is rejected
+     *
+     * @param decision true if the application is accepted / false if the
+     * application is rejected
      * @param justification the Decision's justification
      * @param faeKnowledge the FAE's Knowledge about the Event's topic
      * @param adequacy the Application's adequacy for the Event
-     * @param invitationAdequacy the invitations quantity adequacy for the Application
+     * @param invitationAdequacy the invitations quantity adequacy for the
+     * Application
      * @param overallRecomendation overall recommendation
      */
     public void setDecision(boolean decision, String justification, int faeKnowledge, int adequacy, int invitationAdequacy, int overallRecomendation) {
         d = new Decision(decision, justification, faeKnowledge, adequacy, invitationAdequacy, overallRecomendation);
     }
-    
+
     /**
      * Regists the Decision by adding it to the FAE's Decision list
      */
     public void registDecision() {
-        fae.getDecisionList().getDecisionList().add(d);
+        ((FAE) user).getDecisionList().getDecisionList().add(d);
     }
-    
+
     /**
      * Set's the Application's state as decided
      */
     public void setStateApplication() {
         a.setState(ApplicationState.State.Decided);
     }
-    
+
+    public boolean isOrganizer(User u) {
+        return fc.getOrganizerList().getOrganizerList().contains((Organizer) u);
+    }
+
+    public boolean isFAE(User u) {
+        return fc.getFAEList().getFAEList().contains((FAE) u);
+    }
 }
