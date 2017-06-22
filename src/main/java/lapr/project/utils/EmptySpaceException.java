64a8e32b49651a57e645e477afb105b4efb4f9cc
Diogo <gui.nho0098@gmail.com>
2017-06-22 03:36:41 +0100

    [Implementation] - exception created

---
+++ src/main/java/lapr/project/utils/EmptySpaceException.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.utils;
+
+/**
+ *
+ * @author Diogo
+ */
+public class EmptySpaceException extends IllegalArgumentException {
+
+    /**
+     * Creates a new instance of <code>EmptySpaceException</code> without detail
+     * message.
+     */
+    public EmptySpaceException() {
+    }
+
+    /**
+     * Constructs an instance of <code>EmptySpaceException</code> with the
+     * specified detail message.
+     *
+     * @param msg the detail message.
+     */
+    public EmptySpaceException(String msg) {
+        super("Empty information on parameter "+msg);
+    }
+}
