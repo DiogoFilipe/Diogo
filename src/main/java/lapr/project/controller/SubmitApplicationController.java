216f63e4ef8cb544736eacdf53ff84094815ca72
Diogo <gui.nho0098@gmail.com>
2017-06-23 06:45:53 +0100

    [Implementation] - submit application controller done

--- src/main/java/lapr/project/controller/SubmitApplicationController.java
+++ src/main/java/lapr/project/controller/SubmitApplicationController.java
@@ -3,27 +3,56 @@ package lapr.project.controller;
 import java.util.ArrayList;
 import java.util.List;
 import lapr.project.model.*;
+import lapr.project.utils.EmptySpaceException;
 
 /**
  *
  * @author 1160590_1160795_1160844_1161249_1162109
  */
 public class SubmitApplicationController {
-    
-    FairCenter fc;
-    EventRegist er;
-    Event e;
-    Application a;
-    List<Application> al;
-    
-    public SubmitApplicationController(FairCenter fc) {
+
+    /**
+     * fair center
+     */
+    private FairCenter fc;
+
+    /**
+     * event
+     */
+    private Event e;
+
+    /**
+     * application
+     */
+    private Application a;
+
+    /**
+     * applications list
+     */
+    private List<Application> al;
+
+    /**
+     * user
+     */
+    private User u;
+
+    /**
+     *
+     * @param fc fair center
+     * @param u user
+     */
+    public SubmitApplicationController(FairCenter fc, User u) {
         this.fc = fc;
-        this.er = fc.getEventRegist();
+        this.u = u;
     }
-    
+
+    /**
+     * 
+     * @return list of events opeen for applications
+     */
     public List<String> getListEventsOpenForApplications() {
         List<String> eventsOpenForApplicationsListString = new ArrayList<>();
-        List<Event> eventList = er.getEventList();
+        List<Event> eventList = fc.getEventRegist().getEventList();
         for (Event event : eventList) {
             if (event.getState() == EventState.State.OpenForApplications) {
                 eventsOpenForApplicationsListString.add(event.getTitle());
@@ -31,32 +60,51 @@ public class SubmitApplicationController {
         }
         return eventsOpenForApplicationsListString;
     }
+
+    /**
+     * 
+     * @param companyName name of the company
+     * @param address address of the company
+     * @param contact contact of the company
+     * @param description description
+     * @param area area needed
+     * @param invites number of invites
+     * @param keywordsList list of keywords
+     * @return the new applicaiton
+     */
+    public Application createApplication(String companyName, String address, int contact, String description, double area, int invites, List<Keyword> keywordsList) {
+        Application newApplication = new Application(companyName, address, contact, description, area, invites, keywordsList);
+        return newApplication;
+    }
+
+    /**
+     *
+     * @param e event title
+     * @return list of applications of the event
+     */
+    public List<Application> getApplicationsListOfTheEvent(String e) {
+        return fc.getEvent(e).getApplicationList().getApplicationList();
+    }
     
-    public void getApplicationList(String eventTitle) {
-        List<Event> eventList = er.getEventList();
-        for (Event event : eventList) {
-            if (event.getTitle().equals(eventTitle)) {
-                e = event;
-            }
+        /**
+     *
+     * @param s string given
+     * @param m parameter that can fails
+     */
+    public void validateString(String s, String m) {
+        if (s.trim().equals(" ") || s.equals("")) {
+            throw new EmptySpaceException(m);
         }
-        this.al = e.getApplicationList().getApplicationList();
     }
     
-    public void setData(String companyName, String address, int contact, String description, double boothArea, int invites, List<String> keywordListString) {
-        List<Keyword> keywordList = new ArrayList<>();
-        for (String word : keywordListString) {
-            Keyword keyword = new Keyword(word);
-            keywordList.add(keyword);
+    /**
+     * 
+     * @param s String of a number
+     * @param m parameter that can fail
+     */
+    public void validateNumber(String s,String m){
+        if(s.trim().equals(" ") || s.trim().equals("") || Integer.parseInt(s)<0 || s.contains("[a-zA-Z+]") ){
+            throw new EmptySpaceException(m);
         }
-        a = new Application(companyName, address, contact, description, boothArea, invites, keywordList);
     }
-    
-    public void addApplication() {
-        al.add(a);
-    }
-    
-    public void setCreated() {
-        a.setState(ApplicationState.State.Created);
-    }
-    
 }
