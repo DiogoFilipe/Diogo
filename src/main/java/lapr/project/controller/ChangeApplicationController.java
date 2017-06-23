73d9d9fd17b41f2d512a03fcf8df3979bb07264e
Diogo <gui.nho0098@gmail.com>
2017-06-23 06:14:40 +0100

    [Implementation] - change application controller done

--- src/main/java/lapr/project/controller/ChangeApplicationController.java
+++ src/main/java/lapr/project/controller/ChangeApplicationController.java
@@ -11,6 +11,7 @@ import lapr.project.model.FairCenter;
 import lapr.project.model.Keyword;
 import lapr.project.model.Representative;
 import lapr.project.model.User;
+import lapr.project.utils.EmptySpaceException;
 
 /**
  *
@@ -18,37 +19,97 @@ import lapr.project.model.User;
  */
 public class ChangeApplicationController {
 
-    FairCenter fc;
-    User user;
+    /**
+     * fair center
+     */
+    private FairCenter fc;
+    
+    /**
+     * user
+     */
+    private User user;
 
-    public boolean isRepresentative(User u) {
-        return ((Representative) u).hasCompanyName();
-    }
-
+    /**
+     * 
+     * @param fc fair center
+     * @param user user
+     */
     public ChangeApplicationController(FairCenter fc, User user) {
         this.user = user;
         this.fc = fc;
     }
 
+    /**
+     * 
+     * @return list of events 
+     */
     public List<String> getListEvents() {
         return fc.getEventRegist().getEventListString();
     }
 
+    /**
+     * 
+     * @param e event title
+     * @return list of applications of the representative
+     */
     public List<String> getListApplicationsRepresentative(String e) {
         return fc.getEventRegist().getEvent(e).getRepresentativeListApplications((Representative) user);
     }
 
+    /**
+     * 
+     * @param e event title
+     * @param a application company
+     * @return true if it deletes the application
+     */
     public boolean deleteApplication(String e, String a) {
-        return fc.getEventRegist().getEvent(e).getRepresentativeListApplications((Representative) user).remove(a);
+        return fc.getEventRegist().getEvent(e).getApplicationList().getApplicationList().remove(fc.getEventRegist().getEvent(e).getApplicationList().getApplicationByCompanyName(a));
     }
 
+    /**
+     * 
+     * @param companyName name of the company
+     * @param address address of the company
+     * @param contact contact of the company
+     * @param description description
+     * @param area area needed
+     * @param invites number of invites
+     * @param keywordsList list of keywords
+     * @return the new applicaiton
+     */
     public Application createApplication(String companyName, String address, int contact, String description, double area, int invites, List<Keyword> keywordsList) {
         Application newApplication = new Application(companyName, address, contact, description, area, invites, keywordsList);
         return newApplication;
     }
 
-    public void addToEvent(String e,Application a) {
-        fc.getEvent(e).getApplicationList().getApplicationList().add(a);
+    /**
+     * 
+     * @param e event title
+     * @return list of applications of the event
+     */
+    public List<Application> getApplicationsListOfTheEvent(String e) {
+        return fc.getEvent(e).getApplicationList().getApplicationList();
     }
 
+    /**
+     *
+     * @param s string given
+     * @param m parameter that can fails
+     */
+    public void validateString(String s, String m) {
+        if (s.trim().equals(" ") || s.equals("")) {
+            throw new EmptySpaceException(m);
+        }
+    }
+    
+    /**
+     * 
+     * @param s String of a number
+     * @param m parameter that can fail
+     */
+    public void validateNumber(String s,String m){
+        if(s.trim().equals(" ") || s.trim().equals("") || Integer.parseInt(s)<0 || s.contains("[a-zA-Z+]") ){
+            throw new EmptySpaceException(m);
+        }
+    }
 }
