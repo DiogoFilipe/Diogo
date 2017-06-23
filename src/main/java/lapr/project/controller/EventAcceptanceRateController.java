b3e9d7d7487392c075a93b7fbd18ce2949c3fa9b
Diogo <gui.nho0098@gmail.com>
2017-06-23 11:07:59 +0100

    [Implementation] - event acceptance rate controller done

--- src/main/java/lapr/project/controller/EventAcceptanceRateController.java
+++ src/main/java/lapr/project/controller/EventAcceptanceRateController.java
@@ -10,29 +10,60 @@ import lapr.project.model.User;
  */
 public class EventAcceptanceRateController {
 
-    FairCenter fc;
-    User u;
+    /**
+     * fair center
+     */
+    private FairCenter fc;
 
+    /**
+     * user
+     */
+    private User u;
+
+    /**
+     *
+     * @param fc fair center
+     * @param u user
+     */
     public EventAcceptanceRateController(FairCenter fc, User u) {
         this.fc = fc;
         this.u = u;
     }
-    
-    public List<String> getEventList(){
+
+    /**
+     *
+     * @return list of strings with event title
+     */
+    public List<String> getEventList() {
         return fc.getEventRegist().getEventListString();
     }
 
+    /**
+     *
+     * @param e event
+     * @return list of strings with applications accepted company name
+     */
     public List<String> getApplicationsDecided(String e) {
         return fc.getEvent(e).getApplicationList().getApplicationListAccepted();
     }
 
+    /**
+     *
+     * @param e event title
+     * @return list of strings with application rejected company name
+     */
     public List<String> getApplicationsRejected(String e) {
         return fc.getEvent(e).getApplicationList().getApplicationListRejected();
     }
-    
-    public double acceptanceRate(String e){
+
+    /**
+     *
+     * @param e event title
+     * @return event acceptance rate
+     */
+    public double acceptanceRate(String e) {
         List<String> accepted = getApplicationsDecided(e);
         List<String> rejected = getApplicationsRejected(e);
-        return (accepted.size()/(rejected.size()+accepted.size()))*100;
+        return (accepted.size() / (rejected.size() + accepted.size())) * 100;
     }
 }
