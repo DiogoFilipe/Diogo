e49ddcf930a5ada861dab78691b28f217f710669
Diogo <gui.nho0098@gmail.com>
2017-06-23 08:32:10 +0100

    [Implementation] - assign stand controller alterations

--- src/main/java/lapr/project/controller/AssignStandsController.java
+++ src/main/java/lapr/project/controller/AssignStandsController.java
@@ -22,70 +22,113 @@ import lapr.project.model.User;
  * @author João Domingues
  */
 public class AssignStandsController {
+
+    /**
+     * fair center
+     */
     private FairCenter fc;
+
+    /**
+     * event
+     */
     private Event e;
-    private Organizer o;
+
+    /**
+     * user
+     */
+    private User u;
+
+    /**
+     * list applications
+     */
     private List<Application> applicationList;
+
+    /**
+     * stand
+     */
     private Stand s;
-    private List<Stand>standList;
+
+    /**
+     * list of stands
+     */
+    private List<Stand> standList;
+
+    /**
+     * list assigns
+     */
     private List<AssignStands> assignstands;
 
+    /**
+     * application
+     */
     private Application a;
+
+    /**
+     * assingn stands class
+     */
     private AssignStands as;
 
-    public AssignStandsController(FairCenter fc, Organizer o) {
+    /**
+     *
+     * @param fc fair center
+     * @param u user
+     */
+    public AssignStandsController(FairCenter fc, User u) {
         this.fc = fc;
-        this.o = o;
+        this.u = u;
 
     }
 
     /**
      *
-     * @param a
+     * @param a application
      */
     public void selectApplication(Application a) {
         this.a = a;
 
     }
+
     /**
-     * 
-     * @param e
+     *
+     * @param e event
      * @return stand list not assigned
      */
     public List<Stand> getStands(Event e) {
 
         return this.e.getStandList().getNotAssignedStands();
     }
+
     /**
-     * 
+     *
      * @return stand list
      */
-    public List<Stand> getStandsList(){
-        return standList ; 
+    public List<Stand> getStandsList() {
+        return standList;
     }
-    
+
     /**
      * @return stand selected
      */
     public Stand getStandSelected() {
         return s;
     }
-     
-    
+
     /**
      * Method that returns a events list ready for submission
+     *
      * @return events list active
      */
-    public List<Event> getEventsList () {
+    public List<Event> getEventsList() {
         return fc.getEventRegist().getEventList();
     }
-    
+
     /**
      * @return applications list
      */
     public List<Application> getAppicationsList() {
         return applicationList;
     }
+
     /**
      *
      * @return event
@@ -93,63 +136,62 @@ public class AssignStandsController {
     public Event getEventSelect() {
         return e;
     }
-    
+
     /**
      * @return user selected
      */
     public Application getApplicationSelected() {
         return a;
     }
-    
+
     /**
-     * @return the assigns stands 
+     * @return the assigns stands
      */
     public List<AssignStands> getStandsAssigned() {
         return e.getListAssignedStands().getStandsAssigned();
     }
-    
+
     /**
-     * 
-     * @param u 
+     *
+     * @param a application
      */
-    public void setApplicationSelect(Application a){
+    public void setApplicationSelect(Application a) {
         this.a = a;
     }
-    
+
     /**
      *
-     * @param event
+     * @param event event
      */
     public void setEventSelect(Event event) {
-        this.e = event;    
+        this.e = event;
     }
-    
+
     /**
      *
-     * @param stand
+     * @param stand stand
      */
     public void setStandSelect(Stand stand) {
-        this.s = stand;    
+        this.s = stand;
     }
-    
+
     /**
      *
-     * @param e
-     * @return
+     * @param e event
+     * @return list of applications of the event
      */
     public List<Application> generateApplicationsList(Event e) {
-        
+
         ApplicationList applicationList = new ApplicationList();
-        
 
         return this.applicationList;
     }
 
     /**
      *
-     * @param s
-     * @param a
-     * @return
+     * @param s stand
+     * @param a application
+     * @return the new assignment of the stand with the application
      */
     public AssignStands createAssignment(Stand s, Application a) {
         return e.getListAssignedStands().newStandAssignment(s, a);
@@ -157,8 +199,8 @@ public class AssignStandsController {
 
     /**
      *
-     * @param as
-     * @return
+     * @param as assign stand
+     * @return if the assign stand is valid
      */
     public boolean validate(AssignStands as) {
         return e.getListAssignedStands().validate(as);
@@ -167,8 +209,8 @@ public class AssignStandsController {
 
     /**
      *
-     * @param validate
-     * @param sa
+     * @param validate boolean if is valid
+     * @param sa assign stand
      */
     public void AssignStand(boolean validate, AssignStands sa) {
         if (validate == true) {
@@ -177,5 +219,5 @@ public class AssignStandsController {
         }
 
     }
-    
+
 }
